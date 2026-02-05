edd02d8dfec045e83d3bea43b63698d7436bae64
HainesK-SC <hainesk@sheridancollege.ca>
2026-02-05 17:08:48 -0500

    Update TestClass.java
    Added test comment to test FETCH and MERGE

--- src/ca/sheridancollege/hainesk/main/TestClass.java
+++ src/ca/sheridancollege/hainesk/main/TestClass.java
@@ -2,6 +2,7 @@ package ca.sheridancollege.hainesk.main;
 
 public class TestClass {
 	public void testMethod() {
+		// I'm a comment added from GitHub - FETCH me locally and MERGE into your LOCAL REPO
 		System.out.println("I'm simply a test! If you see me - I work!");
 	}
 }
