11e46f1c5070bc92ab84476843737cf581efc24c
Kyle Haines <hainesk@sheridancollege.ca>
2026-02-05 17:20:22 -0500

    Blah blah blah

--- src/ca/sheridancollege/hainesk/main/TestClass.java
+++ src/ca/sheridancollege/hainesk/main/TestClass.java
@@ -3,6 +3,11 @@ package ca.sheridancollege.hainesk.main;
 public class TestClass {
 	public void testMethod() {
 		// I'm a comment added from GitHub - FETCH me locally and MERGE into your LOCAL REPO
+		/**
+		 * NICE! That worked. Now, let's do the opposite for funsies!
+		 * This JavaDoc comment is being added locally through my IDE. 
+		 * Let's test adding, committing, and pushing to the remote repo! 
+		 */
 		System.out.println("I'm simply a test! If you see me - I work!");
 	}
 }
